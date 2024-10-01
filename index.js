9e01409bfe6932d0372b109a843c67631fe87bea
leonard155 <155899278+leonard155@users.noreply.github.com>
2024-10-01 21:45:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["NATIVE MARKET ON TOP];
+const statusMessages = ["NATIVE MARKET ON TOP","CHEAP SELLING"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
